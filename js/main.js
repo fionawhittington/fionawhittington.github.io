c819c70db559c65d2935bf78bd24386fdcb7aad4
sarah256 <srieger256@gmail.com>
2017-12-26 23:43:29 -0500

    dropdown

--- js/main.js
+++ js/main.js
@@ -77,4 +77,15 @@ $(document).ready(function() {
     	var $this = $(this);
     	$this.text($this.text() == moreText ? lessText : moreText).next("div.more").slideToggle("fast");
 	});
+	
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
 });
