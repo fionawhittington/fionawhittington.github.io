38677a7384fbc528cd4513dd08c4536b61df041e
Helen Zhang <hz96@bu.edu>
2018-03-08 00:23:30 -0500

    updated new landing page

---
+++ oldSite/js/main.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+	// Header Scroll
+	$(window).on('scroll', function() {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 50) {
+			$('#header').addClass('fixed');
+		} else {
+			$('#header').removeClass('fixed');
+		}
+	});
+
+	// Fancybox
+	$('.work-box').fancybox();
+
+	// Flexslider
+	$('.flexslider').flexslider({
+		animation: "fade",
+		directionNav: false,
+	});
+
+	// Page Scroll
+	var sections = $('section')
+		nav = $('nav[role="navigation"]');
+
+	$(window).on('scroll', function () {
+	  	var cur_pos = $(this).scrollTop();
+	  	sections.each(function() {
+	    	var top = $(this).offset().top - 76
+	        	bottom = top + $(this).outerHeight();
+	    	if (cur_pos >= top && cur_pos <= bottom) {
+	      		nav.find('a').removeClass('active');
+	      		nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+	    	}
+	  	});
+	});
+	nav.find('a').on('click', function () {
+	  	var $el = $(this)
+	    	id = $el.attr('href');
+		$('html, body').animate({
+			scrollTop: $(id).offset().top - 75
+		}, 500);
+	  return false;
+	});
+
+	// Mobile Navigation
+	$('.nav-toggle').on('click', function() {
+		$(this).toggleClass('close-nav');
+		nav.toggleClass('open');
+		return false;
+	});	
+	nav.find('a').on('click', function() {
+		$('.nav-toggle').toggleClass('close-nav');
+		nav.toggleClass('open');
+	});
+
+	var wordArray = ["BOSTON", "FEB 16-18"]
+	var indexWords = 0
+
+	function animateWords() {
+		indexWords = (indexWords >= wordArray.length-1) ? 0 :indexWords+1;
+		$('#anim-text').text(function(i,t){
+			return wordArray[indexWords];
+		});
+	}
+
+	animateWords(); //RunOnce
+	setInterval(function(){animateWords()},1800); //thenrepeat
+
+
+	//expand and shrink functionality
+	var moreText = "+ See more",
+    	lessText = "- See less",
+    	moreButton = $("a.readmorebtn");
+
+	moreButton.click(function () {
+    	var $this = $(this);
+    	$this.text($this.text() == moreText ? lessText : moreText).next("div.more").slideToggle("fast");
+	});
+	
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
+});
