835898ef20cd2ea6adc1e033b6dc34bcd5948158
Helen Zhang <hz96@bu.edu>
2018-05-21 16:32:48 -0400

    Outlining the 2019 page
    FAQ / header section

---
+++ js/2019.js
@@ -0,0 +1,12 @@
+
+$(function() {
+$(".text").typed({
+    strings: ["She &lt;Codes&gt; ", "She &lt;Designs&gt; ", "She &lt;Creates&gt; ", "She &lt;Hacks&gt; 2019"],
+    typeSpeed: 50,
+    backSpeed: 5,
+    backDelay: 1500,
+    showCursor: true,
+    loop: false,
+    smartBackspace:true
+  });
+});
